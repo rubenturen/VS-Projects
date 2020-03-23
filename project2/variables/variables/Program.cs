c83dcaf2554bb0296da0df863480c629e94be4b5
BuildTools <unconfigured@null.spigotmc.org>
2020-03-23 12:25:23 +0100

    Added project2

---
+++ project2/variables/variables/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 5;
+            var count = 10;
+            var totalprice = 20.95f;
+            var character = 'a';
+            var firstname = "mosh";
+            var isworking = false;
+
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalprice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstname);
+            Console.WriteLine(isworking);
+        }
+    }
+}
---
+++ project2/variables3/variables3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace variables3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const float Pi = 3.14f;
+            Pi = 1;
+        }
+    }
+}
